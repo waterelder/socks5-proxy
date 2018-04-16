aa7bf896f8626424b8e568fe0ad1e1529945d3fc
Alex Kalina <kalinalex91@gmail.com>
2018-04-17 01:32:05 +0300

    [Deploy] Deploy config was added

---
+++ ecosystem.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  apps: [
+    {
+      name: "socks5-proxy",
+      script: "./index.js",
+      env: {
+        COMMON_VARIABLE: "true"
+      },
+      env_production: {
+        NODE_ENV: "production"
+      }
+    }
+  ],
+  deploy: {
+    stage: {
+      user: "master",
+      host: "92.53.66.193",
+      "ssh_options": "ForwardAgent=yes",
+      ref: "origin/master",
+      repo: "git@github.com:waterelder/socks5-proxy.git",
+      path: "/var/www/socks5-proxy",
+      "post-deploy": "yarn install && pm2 startOrRestart ecosystem.config.js --env production && pm2 save",
+      env: {
+        "NODE_ENV": "production"
+      }
+    },
+  }
+};
